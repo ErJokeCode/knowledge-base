d0535ff6ab6409bcf047e6ffbfd44c7c145d0108
ErJokeCode <eriksoloviev@gmail.com>
2025-04-04 21:51:30 +0500

    first commit

---
+++ src/config.py
@@ -0,0 +1,45 @@
+import json
+import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import BaseModel
+import logging
+from pathlib import Path
+from cryptography.fernet import Fernet
+
+_log = logging.getLogger(__name__)
+
+
+class Settings(BaseSettings):
+    URL_FRONT: str
+
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+
+    PGADMIN_DEFAULT_EMAIL: str
+    PGADMIN_DEFAULT_PASSWORD: str
+
+    def __init__(self):
+        super().__init__(
+            _env_file=".env",
+            _env_file_encoding="utf-8",
+        )
+
+        self.config_logging()
+
+    def config_logging(self, level=logging.INFO) -> None:
+        logging.basicConfig(
+            level=level,
+            datefmt="%Y-%m-%d %H:%M:%S",
+            format="[%(asctime)s.%(msecs)03d] %(module)20s:%(lineno)-3d %(levelname)-7s - %(message)s",
+        )
+
+    @property
+    def DATABASE_URL_asyncpg(self):
+        # postgresql+asyncpg://postgres:postgres@localhost:5432/sa
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
+
+settings = Settings()
---
+++ src/main.py
@@ -0,0 +1,40 @@
+import logging
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
+from config import settings
+
+
+_log = logging.getLogger(__name__)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    _log.info("Start server")
+    yield
+    _log.info("Stop server")
+
+
+app = FastAPI(
+    lifespan=lifespan,
+    root_path="/api"
+)
+
+origins = [
+    settings.URL_FRONT
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# app.include_router()
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
